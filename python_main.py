8680337bac504fd31d9701b96654728d3083b667
fatihbasatemur <fatihbasatemur@gmail.com>
2020-06-22 17:14:28 +0300

    added release_x86 and screenshots

--- python_main.py
+++ python_main.py
@@ -5,6 +5,13 @@ Created on Thu Jun 18 11:21:17 2020
 @author: fbasatemur
 """
 
-from YTube_GUI import Window
-
-window = Window()
+if __name__ == '__main__':
+      import sys
+      from YTube_GUI import Window
+      from PyQt5.QtWidgets import QApplication
+      
+      app = QApplication(sys.argv)
+    
+      windowObject = Window()
+    
+      sys.exit(app.exec_())
